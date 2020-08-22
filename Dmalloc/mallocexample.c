9cead770c8316ec9e23b909d6c2f64606ac75a63
Bob Robey <brobey@lanl.gov>
2020-08-21 20:19:17 -0600

    Fixes to examples

--- Dmalloc/mallocexample.c
+++ Dmalloc/mallocexample.c
@@ -14,5 +14,6 @@ int main(int argc, char *argv[])
    for (int i = 0; i < jmax; i++) {
       x[i] = 0.0;
    }
+   free(x);
    return(0);
 }
--- DrMemory/DrMemory_setup.sh
+++ DrMemory/DrMemory_setup.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-wget https://github.com/DynamoRIO/drmemory/releases/download/release_2.3.0/DrMemory-Linux-2.3.0-1.tar.gz
+wget https://github.com/DynamoRIO/drmemory/releases/download/release_2.3.0/DrMemory-Linux-2.3.0-1.tar.gz && \
+   tar -xzvf DrMemory-Linux-2.3.0-1.tar.gz
+export PATH=`pwd`/DrMemory-Linux-2.3.0-1/bin64:$PATH
